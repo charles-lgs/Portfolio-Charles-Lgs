646fb8fb361aafadae744cfbbec89dd5ab79330d
Charles Langlois <charles.langlois.dev@gmail.com>
2024-11-21 17:11:46 +0100

    Feat/Banner.tsx/menu/burger/mobile

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Menu } from 'lucide-react';
 import NightModeToggle from './NightModeToggle'
 import '../sass/Banner.scss';
 
@@ -9,6 +10,7 @@ interface BannerProps {
 const Banner: React.FC<BannerProps> = ({ onModeToggle }) => {
   const [scrolled, setScrolled] = useState(false);
   const [isDarkMode, setIsDarkMode] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -31,15 +33,22 @@ const Banner: React.FC<BannerProps> = ({ onModeToggle }) => {
     onModeToggle(isDark);
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className={`banner ${scrolled ? 'banner--scrolled' : ''}`}>
       <div className="banner__container">
+        <div className="banner__menu-icon" onClick={toggleMenu}>
+          <Menu />
+        </div>
         <a href="#home">
           <div className={`banner__logo ${scrolled ? 'banner--scrolled-logo' : ''} ${isDarkMode ? 'dark-mode-title-banner' : ''}`}>
             Vizir.dev
           </div>
         </a>
-        <ul className={`banner__links ${scrolled ? 'banner--scrolled-links' : ''} ${isDarkMode ? 'dark-mode-title-banner' : ''}`}>
+        <ul className={`banner__links ${menuOpen ? 'banner__links--open' : ''} ${scrolled ? 'banner--scrolled-links' : ''} ${isDarkMode ? 'dark-mode-title-banner' : ''}`}>
           <li><a href="#about">A propos</a></li>
           <li><a href="#course">Parcours</a></li>
           <li><a href="#skills">Compétences</a></li>
