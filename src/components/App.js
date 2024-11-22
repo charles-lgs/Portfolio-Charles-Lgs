de4b0c992e4e579e1466279b1c69103fe17aa705
Charles Langlois <charles.langlois.dev@gmail.com>
2024-11-22 13:17:46 +0100

    Feat/Loader.tsx

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,27 @@
+import React, { useState, useEffect } from "react";
+import Loader from "./Loader";
 import Portfolio from "./Portfolio.tsx";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loader-container">
+        <Loader />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Portfolio />
